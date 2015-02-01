f0e425fd64f973d63d840ae99532e4067ea85f60
Maico Timmerman <maico.timmerman@gmail.com>
2015-02-01 20:41:14 +0100

    fixed floating point shit and comment

--- ass2/euler_forward.py
+++ ass2/euler_forward.py
@@ -2,19 +2,13 @@
 # Auteurs: Robin Klusman 10675671, Maico Timmerman 10542590
 try:
     import matplotlib.pyplot as plt
-    assert plt
-except ImportError:
-    print("Warning: could not import matplotlib.pyplot")
-try:
     import numpy as np
-    assert np
-except ImportError:
-    print("Warning: could not import numpy")
-try:
     from scipy import ndimage, misc
+    assert np
+    assert plt
     assert ndimage, misc
 except ImportError:
-    print("Warning: could not import scipy.ndimage")
+    print("Warning: could not import scipy, numpy or matplotlib.pyplot")
 
 import linefield
 
@@ -22,14 +16,18 @@ import linefield
 def euler(phi, t0, y0, t1, n, color):
     """
     Get the graph approximation using euler method.
-    """
 
     h = (t1 - t0) / float(n)  # Step width
     t = t0                    # Start t
     y = y0                    # Start y
     tvals = []                # Array of t values
     yvals = []                # Array of y values
-
+    """
+    h = (t1 - t0) / float(n)
+    t = t0
+    y = y0
+    tvals = []
+    yvals = []
     for i in range(n):
         y += h * phi(t, y)
         t += h
@@ -42,7 +40,7 @@ def euler(phi, t0, y0, t1, n, color):
 
 if __name__ == "__main__":
     f1 = lambda t, y: 1 - 2 * t * y
-    f2 = lambda t, y: y * (1 - 1 / 3 * y)
+    f2 = lambda t, y: y * (1 - (1 / 3.) * y)
 
     euler(f1, 0, 1.5, 2, 2000, 'r-')
     euler(f1, 0, .25, 2, 2000, 'm-')
